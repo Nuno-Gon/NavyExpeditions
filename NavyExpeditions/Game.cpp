4e294f84e1581f9f7eca38ef003de58c03b26606
Diogo <diogoplima9@gmail.com>
2018-11-21 21:06:43 +0000

    k
    k

--- NavyExpeditions/Game.cpp
+++ NavyExpeditions/Game.cpp
@@ -10,8 +10,25 @@ void Game::displayMenuTwo(string *comando) {
 	cout << "--- Lista de Comandos Fase 2 --- \n" << endl;
 	cout << endl;
 	cout << "exec <nomeFicheiro>" << endl;
-	//Resto dos comandos
-	//...
+	cout << "prox" << endl;
+	cout << "compranav <T>" << endl;
+	cout << "vendenav <T>" << endl;
+	cout << "lista" << endl;
+	cout << "compra <N><M>" << endl;
+	cout << "vende <N>" << endl;
+	cout << "move <N><X>" << endl;
+	cout << "auto <N>" << endl;
+	cout << "stop <N>" << endl;
+	cout << "pirata <X><Y><T>" << endl;
+	cout << "evpos <E><X><Y>" << endl;
+	cout << "evnav <E><N>" << endl;
+	cout << "moedas <N>" << endl;
+	cout << "vaipara <N><X><Y>" << endl;
+	cout << "comprasold <N><S>" << endl;
+	cout << "saveg <nome>" << endl;
+	cout << "loadg <nome>" << endl;
+	cout << "delg <nome>" << endl;
+	cout << "sair" << endl;
 	cout << endl;
 	cout << "Comando: " << endl;
 	getline(cin, *comando);
