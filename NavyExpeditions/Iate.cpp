d94637925c0b6c13d02f1bb430716ba24dd7b372
Diogo <diogoplima9@gmail.com>
2018-11-26 01:16:09 +0000

    q
    q

---
+++ NavyExpeditions/Iate.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "Iate.h"
+#include "Navio.h"
+
+Iate::Iate() : Navio(3, 0, 0, 10000, 'i') {
+}
+Iate::~Iate()
+{
+}
---
+++ NavyExpeditions/Iate.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "Navio.h"
+
+class Iate : public Navio {
+	Iate();
+
+	~Iate();
+};
