439c82e751ded1ad42e5589f8bad5a684daed693
Diogo <diogoplima9@gmail.com>
2019-01-04 07:35:13 +0000

    1
    1

--- NavyExpeditions/Sereias.cpp
+++ NavyExpeditions/Sereias.cpp
@@ -1,28 +1,14 @@
 #include "stdafx.h"
 #include "Sereias.h"
-#include <ctime>
-#include <vector>
 
 
 Sereias::Sereias(){
 	msg();
-	eventoAcao();
-}
-
-Sereias::Sereias(vector<Navio> navios) {
-	msg();
-	eventoAcao(navios);
 }
 
 void Sereias::msg() {
 	cout << "Ocorreu um Evento: Sereias!" << endl;
 };
-
-void Sereias::eventoAcao(vector<Navio> navios) {
-	srand(time(NULL));
-	int r = rand() % navios.size();
-	navios[r].setSoldados(-navios[r].getSoldados() * 0.1);
-}
 
 Sereias::~Sereias()
 {
--- NavyExpeditions/Sereias.h
+++ NavyExpeditions/Sereias.h
@@ -1,13 +1,10 @@
 #pragma once
 #include "Evento.h"
-#include "Navio.h"
 
 class Sereias : public Evento {
 public:
 	Sereias();
-	Sereias(vector<Navio>);
 	void eventoAcao();
-	void eventoAcao(vector<Navio>);
 	void msg();
 	~Sereias();
 };
